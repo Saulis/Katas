7e8a1c08ddfed886a0f1a31f082bc5f9539e083f
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-05 21:30:29 +0300

    Second test implemented.

--- Scala/src/bowling/GameTests.scala
+++ Scala/src/bowling/GameTests.scala
@@ -1,5 +1,7 @@
 package bowling
 
+import org.junit.Before
+
 /**
  * Created with IntelliJ IDEA.
  * User: Saulis
@@ -12,11 +14,25 @@ class GameTests {
   import org.junit.Assert._
 
   val twentyZeroes = List.fill(20)(0)
+  val twentyOnes = List.fill(20)(1)
+  val game = Game
+
+  @Before
+  def initialize() {
+  }
 
   @Test
   def gutterGame() {
-       val game = Game
-       val points = game.roll(twentyZeroes)
-       assertEquals(0, points)
+    val points = game.roll(twentyZeroes)
+
+    assertEquals(0, points)
+  }
+
+
+  @Test
+  def allOnesGame() {
+    val points = game.roll(twentyOnes)
+
+    assertEquals(20, points)
   }
 }
