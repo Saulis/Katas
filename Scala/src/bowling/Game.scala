04e338177994c9e0717b2e7bab09d92af0cb4a0c
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-05 22:40:23 +0300

    Refactored.

--- Scala/src/bowling/Game.scala
+++ Scala/src/bowling/Game.scala
@@ -1,12 +1,5 @@
 package bowling
 
-/**
- * Created with IntelliJ IDEA.
- * User: Saulis
- * Date: 5.10.2012
- * Time: 20:52
- * To change this template use File | Settings | File Templates.
- */
 object Game {
 
   private def sumOfNextThreeRolls(pins: List[Int]): Int = {
@@ -17,40 +10,37 @@ object Game {
     pins.take(2).sum
   }
 
-  private def lastFrameEndsWithStrike(pins: List[Int]): Boolean = {
-    pins.length == 3 & nextRollIsStrike(pins)
-  }
-
   private def lastFrame(frame: Int): Boolean = frame == 10
 
   private def lastFrameTotal(pins: List[Int]): Int = {
     pins.sum
   }
 
-  private def frameTotal(frame: Int, pins: List[Int]): Int = {
+  private def nextFramesTotal(frame: Int, pins: List[Int]): Int = {
     if (lastFrame(frame))
       lastFrameTotal(pins)
-    else if (nextRollIsStrike(pins))
-      sumOfNextThreeRolls(pins) + frameTotal(frame + 1, pins.drop(1))
-    else if (frameEndsWithSpare(pins))
-      sumOfNextThreeRolls(pins) + frameTotal(frame + 1, pins.drop(2))
     else
-      sumOfNextTwoRolls(pins) + frameTotal(frame + 1, pins.drop(2))
+      frameTotal(frame, pins)
   }
 
-  private def nextRollIsStrike(pins: List[Int]) : Boolean = {
+  private def frameTotal(frame: Int, pins: List[Int]): Int = {
+    if (firstRollIsStrike(pins))
+      sumOfNextThreeRolls(pins) + nextFramesTotal(frame + 1, pins.drop(1))
+    else if (frameEndsWithSpare(pins))
+      sumOfNextThreeRolls(pins) + nextFramesTotal(frame + 1, pins.drop(2))
+    else
+      sumOfNextTwoRolls(pins) + nextFramesTotal(frame + 1, pins.drop(2))
+  }
+
+  private def firstRollIsStrike(pins: List[Int]) : Boolean = {
     pins.head == 10
   }
 
   private def frameEndsWithSpare(pins: List[Int]) : Boolean = {
-    (!nextRollIsStrike(pins) & sumOfNextTwoRolls(pins) == 10)
-  }
-
-  private def pinsRemain(pins: List[Int]): Boolean = {
-    pins.length > 0
+    (!firstRollIsStrike(pins) & sumOfNextTwoRolls(pins) == 10)
   }
 
   def roll (pins: List[Int]): Int =  {
-    frameTotal(1, pins)
+    nextFramesTotal(1, pins)
    }
 }
--- Scala/src/bowling/GameTests.scala
+++ Scala/src/bowling/GameTests.scala
@@ -1,15 +1,7 @@
 package bowling
 
-import org.junit.Before
-
-/**
- * Created with IntelliJ IDEA.
- * User: Saulis
- * Date: 5.10.2012
- * Time: 20:54
- * To change this template use File | Settings | File Templates.
- */
 class GameTests {
+  import org.junit.Before
   import org.junit.Test
   import org.junit.Assert._
 
