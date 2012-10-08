5f8c3a80d461a692813e634d38450e6105f38d11
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-08 22:31:46 +0300

    Perfect game added.

--- FSharp/Tests/BowlingTests.fs
+++ FSharp/Tests/BowlingTests.fs
@@ -10,6 +10,7 @@ module BowlingTests =
     let twentyOnes = Array.create 20 1
     let spareRolls = Array.append [|5; 5; 3|] (Array.create 17 0)
     let strikeRolls = Array.append [|10; 3; 4|] (Array.create 16 0)
+    let perfectRolls = Array.create 12 10
 
     [<TestFixture>] 
     type ``Given a game!`` ()=
@@ -30,3 +31,7 @@ module BowlingTests =
         [<Test>] member strikeGame.
          ``strike game should have 24 points`` () =
             game.GetScore(strikeRolls) |> should equal 24
+
+        [<Test>] member perfectGame.
+         ``perfect game should have 300 points`` () =
+            game.GetScore(perfectRolls) |> should equal 300
