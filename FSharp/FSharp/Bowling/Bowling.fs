1af1174feecc712bde28ebcfcfb75165c04cf9c3
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-08 21:46:05 +0300

    First two tests on FSharp.

---
+++ FSharp/FSharp/Bowling/Bowling.fs
@@ -0,0 +1,6 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module Bowling
+
+type Game() = 
+    member x.GetScore (rolls : int[]) = Array.sum rolls
---
+++ FSharp/Tests/BowlingTests.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.net
+
+namespace Tests.Tests
+
+open NUnit.Framework
+open FsUnit
+open Bowling
+
+module BowlingTests =
+
+    let twentyZeroes = Array.create 20 0
+    let twentyOnes = Array.create 20 1
+
+    [<TestFixture>] 
+    type ``Given a game!`` ()=
+        let game = new Game()
+
+        [<Test>] member gutterBallGame.
+         ``gutter ball game should have zero points`` ()=
+            game.GetScore(twentyZeroes) |> should equal 0
+
+        [<Test>] member onesGame.
+         ``ones game should have twenty points`` ()=
+            game.GetScore(twentyOnes) |> should equal 20
