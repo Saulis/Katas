0cadad3b545d16cecf92befeae9605ccafbe4441
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-20 15:17:00 +0300

    Refactored BowlingGame.

--- Haskell/src/Bowling/BowlingGame.hs
+++ Haskell/src/Bowling/BowlingGame.hs
@@ -3,6 +3,12 @@ module BowlingGame (score) where
 score :: [Int] -> Int
 score rolls = sumFrames (mapIntoTenFrames rolls)
 
+mapIntoTenFrames rolls = foldIntoTenFrames (mapIntoFrames rolls)
+foldIntoTenFrames frames = (firstNine frames) ++ [foldTenthFrameFrom frames]
+
+firstNine frames = take 9 frames
+foldTenthFrameFrom frames = (foldl (++) [] (drop 9 frames))
+
 mapIntoFrames :: [Int] -> [[Int]]
 mapIntoFrames [] = []
 mapIntoFrames (x:xs)
@@ -10,17 +16,15 @@ mapIntoFrames (x:xs)
     | length xs == 0 = [[x]]
     | otherwise = [[x, (head xs)]] ++ mapIntoFrames (tail xs)
 
-firstNine frames = take 9 frames
-foldTenthFrameFrom frames = (foldl (++) [] (drop 9 frames))
-foldIntoTenFrames frames = (firstNine frames) ++ [foldTenthFrameFrom frames]
-mapIntoTenFrames rolls = foldIntoTenFrames (mapIntoFrames rolls)
-
 sumFrames :: [[Int]] -> Int
 sumFrames [] = 0
-sumFrames (x:xs)
-    | head x == 10 = sum x + sumOfNextTwoRolls xs + sumFrames xs --strike
-    | sum x == 10 = sum x + nextRoll xs + sumFrames xs -- spare
-    | otherwise = sum x + sumFrames xs
+sumFrames (frame:nextFrames)
+    | endsWithStrike frame = sum frame + sumOfNextTwoRolls nextFrames + sumFrames nextFrames
+    | endsWithSpare frame = sum frame + nextRoll nextFrames + sumFrames nextFrames
+    | otherwise = sum frame + sumFrames nextFrames
+
+endsWithStrike frame = head frame == 10
+endsWithSpare frame = sum frame == 10
 
 nextRoll :: [[Int]] -> Int
 nextRoll [] = 0
@@ -28,6 +32,6 @@ nextRoll frames = head (head frames)
 
 sumOfNextTwoRolls :: [[Int]] -> Int
 sumOfNextTwoRolls [] = 0
-sumOfNextTwoRolls frames
-    | length (head frames) >= 2 = sum (take 2 (head frames))
-    | otherwise = nextRoll frames + nextRoll (tail frames)
+sumOfNextTwoRolls (frame:nextFrames)
+    | length frame >= 2 = sum (take 2 frame)
+    | otherwise = nextRoll [frame] + nextRoll nextFrames
