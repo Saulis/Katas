8a3605978b93f5c02ec1823743ad82dfb22a3bc6
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-20 14:30:29 +0300

    Added remaining tests.

--- Haskell/src/Bowling/BowlingGame.hs
+++ Haskell/src/Bowling/BowlingGame.hs
@@ -1,4 +1,33 @@
-module BowlingGame (score) where
+
 
 score :: [Int] -> Int
-score rolls = sum rolls
+score rolls = sumFrames (mapIntoTenFrames rolls)
+
+mapIntoFrames :: [Int] -> [[Int]]
+mapIntoFrames [] = []
+mapIntoFrames (x:xs)
+    | x == 10 = [[x]] ++ mapIntoFrames xs
+    | length xs == 0 = [[x]]
+    | otherwise = [[x, (head xs)]] ++ mapIntoFrames (tail xs)
+
+firstNine frames = take 9 frames
+foldTenthFrameFrom frames = (foldl (++) [] (drop 9 frames))
+foldIntoTenFrames frames = (firstNine frames) ++ [foldTenthFrameFrom frames]
+mapIntoTenFrames rolls = foldIntoTenFrames (mapIntoFrames rolls)
+
+sumFrames :: [[Int]] -> Int
+sumFrames [] = 0
+sumFrames (x:xs)
+    | head x == 10 = sum x + sumOfNextTwoRolls xs + sumFrames xs --strike
+    | sum x == 10 = sum x + nextRoll xs + sumFrames xs -- spare
+    | otherwise = sum x
+
+nextRoll :: [[Int]] -> Int
+nextRoll [] = 0
+nextRoll frames = head (head frames)
+
+sumOfNextTwoRolls :: [[Int]] -> Int
+sumOfNextTwoRolls [] = 0
+sumOfNextTwoRolls frames
+    | length (head frames) >= 2 = sum (take 2 (head frames))
+    | otherwise = nextRoll frames + nextRoll (tail frames)
--- Haskell/src/Bowling/BowlingGameTests.hs
+++ Haskell/src/Bowling/BowlingGameTests.hs
@@ -1,10 +1,14 @@
-module BowlingGameTests (main) where
-
 import Test.HUnit
 import BowlingGame
 
 twentyZeroes = replicate 20 0
 twentyOnes = replicate 20 1
+spareGameRolls = [5,5,3] ++ replicate 17 0
+strikeGameRolls = [10, 3, 4] ++ replicate 16 0
+perfectRoll = replicate 12 10
+almostPerfectGameRolls = replicate 9 10 ++ [5,5]
+lastMomentFailGameRolls = replicate 10 10 ++ [5,5]
+
 
 gutterGameTest = TestCase $ assertEqual
   "Should get zero points from twenty gutter balls" 0 (BowlingGame.score twentyZeroes)
@@ -12,8 +16,28 @@ gutterGameTest = TestCase $ assertEqual
 onesGameTest = TestCase $ assertEqual
   "Should get twenty points from twenty hits" 20 (BowlingGame.score twentyOnes)
 
+spareGameTest = TestCase $ assertEqual
+  "Should get sixteen points from spare game" 16 (BowlingGame.score spareGameRolls)
+
+strikeGameTest = TestCase $ assertEqual
+  "Should get 24 points from strike game" 24 (BowlingGame.score strikeGameRolls)
+
+perfectGameTest = TestCase $ assertEqual
+  "Should get 300 points from perfect game" 300 (BowlingGame.score perfectRolls)
+
+almostPerfectGameTest = TestCase $ assertEqual
+  "Should get 265 points from almost perfect game" 265 (BowlingGame.score almostPerfectGameRolls)
+
+lastMomentFailGameTest = TestCase $ assertEqual
+  "Should get 285 points from last moment fail game" 285 (BowlingGame.score lastMomentFailGameRolls)
+
 tests = TestList [TestLabel "Gutter Game" gutterGameTest,
-                  TestLabel "Ones Game" onesGameTest]
+                  TestLabel "Ones Game" onesGameTest,
+                  TestLabel "Spare Game" spareGameTest,
+                  TestLabel "Strike Game" strikeGameTest,
+                  TestLabel "Perfect Game" perfectGameTest,
+                  TestLabel "Almost Perfect Game" almostPerfectGameTest,
+                  TestLabel "Last Moment Fail Game" lastMomentFailGameTest]
 
 main = runTestTT tests
 
