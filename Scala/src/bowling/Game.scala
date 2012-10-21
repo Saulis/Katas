50a519f786a9f1817fb1b4219befd2fef7006265
Sauli Tähkäpää <sauli.tahkapaa@netti.fi>
2012-10-21 22:12:24 +0300

    Polished up.

--- Scala/src/bowling/Game.scala
+++ Scala/src/bowling/Game.scala
@@ -20,26 +20,22 @@ object Game {
     List(frames.reduceLeft[List[Int]] { (acc, n) => acc ++ n })
   }
   
-  private def mapIntoFrames (rolls: List[Int]): List[List[Int]] = {
-	if(rolls.isEmpty)  
-		List()
-	
-	else if(rolls.length == 1)
-	  List(List(rolls.head))
-		
-    else if(rolls.head == 10)
-	    List(List(rolls.head)) ++ mapIntoFrames(rolls.tail)
+  private def mapIntoFrames (rolls: List[Int]): List[List[Int]] = rolls.length match {
+    case 0 => List()
+    case 1 => List(List(rolls.head))
+    case _ => 
+      if(frameEndsWithStrike(rolls))
+    	  List(List(rolls.head)) ++ mapIntoFrames(rolls.tail)
 	    else
 	      List(rolls.take(2)) ++ mapIntoFrames(rolls.drop(2))
   }
   
-  private def sumFrames (frames: List[List[Int]]): Int = {
-    if(frames.isEmpty)
-      0
-      else if (frames.length == 1)
-        frames.head.sum
-      else if (frameEndsWithStrike(frames.head))
-      frames.head.sum + sumOfNextTwoRolls(frames.tail) + sumFrames(frames.tail)
+  private def sumFrames (frames: List[List[Int]]): Int = frames.length match {
+    case 0 => 0
+    case 1 => frames.head.sum
+    case _ => 
+      if (frameEndsWithStrike(frames.head))
+    	frames.head.sum + sumOfNextTwoRolls(frames.tail) + sumFrames(frames.tail)
       else if(frameEndsWithSpare(frames.head))
         frames.head.sum + nextRoll(frames.tail) + sumFrames(frames.tail)
         else
@@ -57,4 +53,4 @@ object Game {
   private def frameEndsWithSpare(frame: List[Int]) : Boolean = !frameEndsWithStrike(frame) & frame.sum == 10
   private def nextRoll(frames: List[List[Int]]): Int = frames.head.head
   
-  }
+}
